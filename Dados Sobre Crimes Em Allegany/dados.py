5df56e7236b9b00ba9198d4ca585984abd5ad467
EduardoDonini <145741177+EduardoDonini@users.noreply.github.com>
2024-07-13 15:01:26 -0300

    main.py

--- Dados Sobre Crimes Em Allegany/dados.py
+++ Dados Sobre Crimes Em Allegany/dados.py
@@ -8,9 +8,11 @@ dados = pd.read_csv("Dados Sobre Crimes Em Allegany/MD_Crime_Data.csv", sep=";")
 
 assaltos = dados["ASSALTOS"].to_list()
 ano = dados["YEAR"].to_list()
+assasinatos = dados["MURDER"].to_list()
 
 n_assaltos = assaltos
 ano_indice = ano
+n_assasinatos = assasinatos
 
 serie = pd.Series(n_assaltos,ano_indice)
 
@@ -25,10 +27,19 @@ def grafico_linhas():
     plt.grid(True)
     plt.show()
 
+def grafico_barras():
+    plt.title("NUMERO DE ASSASINATOS EM ALLEGANY COUNTY AO LONGO DOS ANOS")
+    plt.xlabel("ANOS")
+    plt.ylabel("NUMERO DE ASSALTOS")
+    plt.bar(ano_indice,n_assasinatos)
+    plt.xticks(range(min(ano), max(ano)+1, 5))
+    plt.grid(True)
+    plt.show()
 
 janela = Tk()
-janela.title("Análise de Assaltos")
+janela.title("ANALISE DE CRIMES EM ALLEGANY")
 
-Button(janela, text="Analisar Gráfico", command=grafico_linhas).pack()
+Button(janela, text="Analisar Assaltos", command=grafico_linhas).pack()
+Button(janela, text="Analisar Assasinatos", command=grafico_barras).pack()
 
 mainloop()
